302771af9c2c8328caf0f1c37644e41732bc33a5
ShrinivasDhole <shrinivasdhole7@gmail.com>
2025-02-11 10:51:42 +0530

    doctor flow added

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AdminContextProvider from "./context/AdminContext.jsx";
+import DoctorContextProvider from "./context/DoctorContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-   
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
           <App />
-  </BrowserRouter>,
-)
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
+  </BrowserRouter>
+);
